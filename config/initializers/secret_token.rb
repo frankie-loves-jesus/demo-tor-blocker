108c9d06a8c90c1d6d5e59bb8a36357cfc92d015
Frankie <orangeprince@hushmail.com>
2014-03-21 02:53:43 +0100

    rails new demo-tor-blocker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoTorBlocker::Application.config.secret_key_base = '6de1d1007474c5007b48d94bebae8dfe6f07518fdeccc03fc85e4922ff63eae96e93261de6c1472955cabf65fafa238f5b24a5f2795a1ecd8016c168d8903214'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoTorBlocker::Application.config.session_store :cookie_store, key: '_demo-tor-blocker_session'
